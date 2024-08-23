c6198114e24bff3632d6669168a7bec95a1f7531
hendisantika <hendisantika@yahoo.co.id>
2024-08-23 08:02:48 +0700

    feat: Add TestMain test 🫸🌀✏️📗🐧🐳

--- infra/testcontainers_test.go
+++ infra/testcontainers_test.go
@@ -1,8 +1,16 @@
 package infra
 
 import (
+	"context"
 	"github.com/testcontainers/testcontainers-go"
 	"go-testcontainers-sample/domain"
+	"go-testcontainers-sample/infra/mem"
+	"go-testcontainers-sample/infra/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+	"testing"
+	"time"
 )
 
 type image struct {
@@ -22,4 +30,33 @@ var (
 type container struct {
 	testcontainers.Container
 	URI string
+}
+
+func TestMain(m *testing.M) {
+	var code = 1
+	defer func() { os.Exit(code) }()
+
+	ctx := context.Background()
+	mongoContainer, err := setup(ctx, mongoDB)
+	if err != nil {
+		log.Printf("Unexpected error, fallback to mem repository implementations.\nerror: %s.\n", err)
+		allGames = mem.NewAllGames()
+	} else {
+		ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
+		client, err := mdriver.Connect(ctx, options.Client().ApplyURI(mongoContainer.URI))
+		if err != nil {
+			log.Printf("Could not connect to mongodb, fallback to mem repository implementations.\nerror: %s.\n", err)
+			allGames = mem.NewAllGames()
+		} else {
+			allGames = mongo.NewAllGames(client)
+		}
+		defer func() {
+			if err = client.Disconnect(ctx); err != nil {
+				panic(err)
+			}
+		}()
+	}
+
+	code = m.Run()
 }
