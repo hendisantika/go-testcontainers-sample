40f4bbffe0ac43b8ddf9e2d1c3c0dfc20f10685b
hendisantika <hendisantika@yahoo.co.id>
2024-08-23 07:57:45 +0700

    Add Game struct 🫸🌀✏️📗🐧🐳

--- domain/games.go
+++ domain/games.go
@@ -2,3 +2,17 @@ package domain
 
 type PEGI int
 type GameId string
+
+const (
+	Three PEGI = iota
+	Seven
+	Twelve
+	Sixteen
+	Eighteen
+)
+
+type Game struct {
+	Id    GameId
+	Title string
+	PEGI  PEGI
+}
