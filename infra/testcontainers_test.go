dda80b0fa31aed4df22337023926b8e4788e8e35
hendisantika <hendisantika@yahoo.co.id>
2024-08-23 08:02:23 +0700

    feat: Add var test 🫸🌀✏️📗🐧🐳

--- infra/testcontainers_test.go
+++ infra/testcontainers_test.go
@@ -1,6 +1,17 @@
 package infra
 
+import "go-testcontainers-sample/domain"
+
 type image struct {
 	name string
 	port string
 }
+
+var (
+	allGames domain.AllGames
+
+	mongoDB = image{
+		name: "mongo:5.0.8",
+		port: "27017",
+	}
+)
