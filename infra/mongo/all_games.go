a97c7b5ef0c7ba5031e7f6310f03badfe71598d9
hendisantika <hendisantika@yahoo.co.id>
2024-08-23 08:00:30 +0700

    feat: Add NewAllGames mongo 🫸🌀✏️📗🐧🐳

--- infra/mongo/all_games.go
+++ infra/mongo/all_games.go
@@ -6,3 +6,8 @@ type AllGames struct {
 	client *mongo.Client
 	coll   *mongo.Collection
 }
+
+func NewAllGames(client *mongo.Client) *AllGames {
+	collection := client.Database("testing").Collection("games")
+	return &AllGames{client: client, coll: collection}
+}
