ee7e2a565f1bd88e5c6b09b720206280c409172d
hendisantika <hendisantika@yahoo.co.id>
2024-08-23 08:05:02 +0700

    feat: Add Test_Replace test 🫸🌀✏️📗🐧🐳

--- infra/all_games_test.go
+++ infra/all_games_test.go
@@ -20,3 +20,23 @@ func Test_Add(t *testing.T) {
 	assert.Equal(t, "Assassin's Creed Valhalla", game.Title)
 	assert.Equal(t, domain.Eighteen, game.PEGI)
 }
+
+func Test_Replace(t *testing.T) {
+	gameId := domain.GameId(uuid.NewString())
+	allGames.Add(&domain.Game{
+		Id:    gameId,
+		Title: "Rayman Raving Rabbids",
+		PEGI:  domain.Seven,
+	})
+
+	allGames.Add(&domain.Game{
+		Id:    gameId,
+		Title: "Rainbow Six Siege",
+		PEGI:  domain.Eighteen,
+	})
+
+	game := allGames.By(gameId)
+	assert.Equal(t, gameId, game.Id)
+	assert.Equal(t, "Rainbow Six Siege", game.Title)
+	assert.Equal(t, domain.Eighteen, game.PEGI)
+}
